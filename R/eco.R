e5fc724c8218f4809b57594d9bf4ad1171eb6b57
KMS131C4\wojci <w.rosa@pollub.pl>
2019-06-18 10:30:12 +0200

    Added eco dataset

---
+++ R/eco.R
@@ -0,0 +1,18 @@
+#' ECO
+#'
+#' A dataset containing 2014 ECO (Encyclopedia of Chess Openings) openings
+#' \itemize{
+#'   \item ECO
+#'   \item Opening
+#'   \item Variation
+#'   \item Movetext: Standard algebraic notation
+#'   \item NMoves
+#'   \item LAN: Movetext converted into long algebraic notation
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name eco
+#' @usage data(eco)
+#' @format A data frame with ECO openings
+"eco"
